55ee02a13bfa53010a6788ec17aaf7addc6f9c23
ThaiLe1220 <lehongthai2000@gmail.com>
2023-08-16 15:45:11 +0700

    Initial Commit

---
+++ monopoly/monopolyApp.swift
@@ -0,0 +1,17 @@
+//
+//  monopolyApp.swift
+//  monopoly
+//
+//  Created by Lê Ngọc Trâm on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct monopolyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
